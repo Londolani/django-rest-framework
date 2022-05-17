5e2167fd17527e89d834d078a13e83ebeee610e3
Londolani <londolani0700@gmail.com>
2022-05-17 13:51:41 +0200

    first commit

---
+++ drfapp/serializers.py
@@ -0,0 +1,11 @@
+from dataclasses import field
+from rest_framework import serializers
+from .models import Student
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = '__all__'
+        field = (
+            'name','age'
+        )
